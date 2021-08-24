b495b8099869aab3f1449cf0620f729a693f6282
aaronli <aaron@aifian.com>
2021-08-24 14:08:58 +0800

    add Wouter and React Query

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { useDispatch } from 'react-redux'
+import rootReducer from './rootReducer'
 
 export const store = configureStore({
     reducer: {
-
+        reducer: rootReducer,
     },
 })
 
